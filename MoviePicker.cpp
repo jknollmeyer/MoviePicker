c9bf4ee0303ca7e70653643ea413cd5d885a276d
jaketf <jaketf@bu.edu>
2015-03-27 14:37:23 -0400

    Create MoviePicker.cpp

---
+++ MoviePicker.cpp
@@ -0,0 +1,22 @@
+#include "MoviePicker.h"
+#include "userinput.h"
+
+using namespace std;
+
+int main(){
+    
+    initialfileopen();
+    
+    settings settingspane;
+    
+    int num_watchers;
+    
+    vector<userinput> watchers (num_watchers);
+    
+    for (int i= 0; i < num_watchers; i++){
+        watchers[i] = userinput();
+    }
+    
+    // Just barely started
+    
+}
