4c6bb9caab892a68bff5f90ad274aa4b0b6c8dc3
jknollmeyer <jaknollmeyer@gmail.com>
2015-04-25 17:56:55 -0400

    configured file I/O to work with final format

--- app/src/main/java/ec327/moviepicker/OutputActivity.java
+++ app/src/main/java/ec327/moviepicker/OutputActivity.java
@@ -6,6 +6,8 @@ import android.support.v7.app.ActionBarActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.EditText;
+
 
 import java.io.BufferedReader;
 import java.io.InputStream;
@@ -24,8 +26,8 @@ public class OutputActivity extends ActionBarActivity {
         Button ReturnButton = (Button)findViewById(R.id.ReturnButton);
         ReturnButton.setOnClickListener(new View.OnClickListener(){
             public void onClick(View v){
-                    Intent returnScreen =  new Intent(getApplicationContext(),MainActivity.class);
-                    startActivity(returnScreen);
+                Intent returnScreen =  new Intent(getApplicationContext(),MainActivity.class);
+                startActivity(returnScreen);
             }
         });
 
@@ -38,15 +40,52 @@ public class OutputActivity extends ActionBarActivity {
         //Set up BufferReader using InputStream from text file at raw/testfile.txt
         InputStream ins = getResources().openRawResource(
                 getResources().getIdentifier("raw/testfile","raw",getPackageName()));
-        BufferedReader reader = new BufferedReader(new InputStreamReader(ins));
+        BufferedReader movieReader = new BufferedReader(new InputStreamReader(ins));
         try{
-            MovieOutput.setText(reader.readLine());
+            //Current Ans 0 = genre CurrentAns 1 = movie ouput
+            String[] currentAns = {"",""};
+            for (int ii=0;ii<5;ii++) {
+                String[] parts = movieReader.readLine().split("\\\t");
+                String[] genres = parts[3].split(" ");
+                System.out.println(parts[parts.length-1]);
+                for(int j = 0; j < genres.length; j++) {
+                    if (genres[j].equals(i.getStringExtra("genre"))) {
+                        currentAns[0] = genres[j];
+                        currentAns[1] = parts[1];
+                    }
+                }
+            }
+            MovieOutput.setText(currentAns[1]);
+            GenreOutput.setText(currentAns[0]);
         }catch(Exception e){
             System.out.println("InputStream Exception");
         }
-
-
-        GenreOutput.setText(i.getStringExtra("genre"));
+        /*
+        get modal genre "mg"
+        get movies of mg
+        pick a movie
+        return the title&year
+         */
+        //(EditText)findViewById(R.id.GenreInput)
+        //getText().toString()
+        //GenreOutput.setText(i.getStringExtra("genre"));
 
     }
 }
+/*
+                getResources().getIdentifier("raw/testfile","raw",getPackageName()));
+            String[] currentAns = {"",""};
+            for (int ii=0;ii<5;ii++) {
+                String[] parts = reader.readLine().split("\\\t");
+//                MovieOutput.setText(parts[0]);
+//                GenreOutput.setText(parts[parts.length - 1]);
+                String[] arr = getResources().getStringArray(R.array.genre_array);
+//                ActorOutput.setText(arr[0]);
+                if (parts[parts.length-1].equals("Horror")) {
+                    currentAns[0] = parts[parts.length-1];
+                    currentAns[1] = parts[0];
+                }
+            }
+            MovieOutput.setText(currentAns[1]);
+            GenreOutput.setText(currentAns[0]);
+ */
